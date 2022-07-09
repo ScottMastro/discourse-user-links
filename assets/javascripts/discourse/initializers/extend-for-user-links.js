ce9cbe10ec58e3ebe5647c886cf399de1c8f5c70
ScottMastro <scott-mastro@hotmail.com>
2022-07-09 01:59:59 -0400

    initial commit

---
+++ assets/javascripts/discourse/initializers/extend-for-user-links.js
@@ -0,0 +1,94 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { isEmpty } from "@ember/utils";
+const { iconNode } = require("discourse-common/lib/icon-library");
+
+function getMatches(string, regex, index) {
+  if (isEmpty(string)){ return []; }
+  index || (index = 1); // default to the first capturing group
+  var matches = [];
+  var match;
+  while (match = regex.exec(string)) {
+    matches.push(match[index]);
+  }
+  return matches;
+}
+
+function addSetting(api) {
+  api.modifyClass("controller:preferences/profile", {
+    pluginId: "discourse-user-links",
+
+    actions: {
+      save() {
+        this.get("saveAttrNames").push("custom_fields");
+        this._super();
+      },
+    },
+  });
+}
+
+export default {
+  name: "extend-for-user-links",
+  initialize(container) {
+    const siteSettings = container.lookup("site-settings:main");
+
+    withPluginApi("0.1", (api) => {
+
+      api.includePostAttributes("collection_thread");
+      api.includePostAttributes("wtb_thread");
+      api.includePostAttributes("ebay_username");
+      api.includePostAttributes("instagram_username");
+      api.includePostAttributes("youtube_username");
+    
+      const isMobileView = container.lookup("site:main").mobileView;
+      const location = isMobileView ? "before" : "after";
+      console.log(location)
+      api.decorateWidget(`poster-name:after`, (dec) => {
+        const attrs = dec.attrs;
+    
+        if (isEmpty(attrs.collection_thread) && isEmpty(attrs.wtb_thread) &&
+            isEmpty(attrs.ebay_username) && isEmpty(attrs.instagram_username) &&
+            isEmpty(attrs.youtube_username)) {
+          return;
+        }
+    
+        var buttons = []
+        
+        var collection_thread_match = getMatches(attrs.collection_thread, /\/t\/[^\/]*\/(\d+)[\/]?/g);
+        if (collection_thread_match.length > 0){
+          var collection_thread_id = collection_thread_match[0];
+          var button = dec.h('a.icon', { href:"/t/"+collection_thread_id, title: 'Collection' }, iconNode('hand-holding-heart'));
+          buttons.push(button);
+        }
+        var wtb_thread_match = getMatches(attrs.wtb_thread, /\/t\/[^\/]*\/(\d+)[\/]?/g);
+        if (wtb_thread_match.length > 0){
+          var wtb_thread_id = wtb_thread_match[0];
+          var button = dec.h('a.icon', { href:"/t/"+wtb_thread_id, title: 'WTB' }, iconNode('comment-dollar'));
+          buttons.push(button);
+        }
+        var instagram_match = getMatches(attrs.instagram_username, /([\d\._a-zA-Z]+)/g);
+        if (instagram_match.length > 0){
+          var instagram_name = instagram_match[0];
+          var button = dec.h('a.icon', { href:"https://www.instagram.com/"+instagram_name, title: 'Instagram' }, iconNode('fab-instagram'));
+          buttons.push(button);
+        }
+        var youtube_match = getMatches(attrs.youtube_username, /([^@&'\(\)<>\s\\\/]+)/g);
+        if (youtube_match.length > 0){
+          var youtube_name = youtube_match[0];
+          var button = dec.h('a.icon', { href:"https://www.youtube.com/c/"+youtube_name, title: 'YouTube' }, iconNode('fab-youtube'));
+          buttons.push(button);
+        }
+        var ebay_match = getMatches(attrs.ebay_username, /([^@&'\(\)<>\s\\\/]+)/g);
+        if (ebay_match.length > 0){
+          var ebay_name = ebay_match[0];
+          var button = dec.h('a.icon', { href:"https://www.ebay.com/usr/"+ebay_name, title: 'Ebay' }, iconNode('fab-ebay'));
+          buttons.push(button);
+        }
+    
+          return [ dec.h("div.user-links", buttons) ];
+      });
+
+    });
+  
+    withPluginApi("0.1", (api) => addSetting(api, siteSettings));
+  },
+};
---
+++ plugin.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+# name: discourse-user-links
+# about: Adds user links to Discourse posts
+# version: 0.1
+# author: Scott Mastro
+# url: https://github.com/ScottMastro/
+# transpile_js: true
+
+enabled_site_setting :user_links_enabled
+
+register_svg_icon "fab-instagram" if respond_to?(:register_svg_icon)
+register_svg_icon "fab-ebay" if respond_to?(:register_svg_icon)
+register_svg_icon "fab-youtube" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-comment-dollar" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-hand-holding-heart" if respond_to?(:register_svg_icon)
+
+
+DiscoursePluginRegistry.serialized_current_user_fields << "collection_thread"
+DiscoursePluginRegistry.serialized_current_user_fields << "wtb_thread"
+DiscoursePluginRegistry.serialized_current_user_fields << "instagram_username"
+DiscoursePluginRegistry.serialized_current_user_fields << "youtube_username"
+DiscoursePluginRegistry.serialized_current_user_fields << "ebay_username"
+
+after_initialize do
+  User.register_custom_field_type('collection_thread', :text)
+  User.register_custom_field_type('wtb_thread', :text)
+  User.register_custom_field_type('instagram_username', :text)
+  User.register_custom_field_type('youtube_username', :text)
+  User.register_custom_field_type('ebay_username', :text)
+
+  register_editable_user_custom_field [:collection_thread, :wtb_thread, :instagram_username, :youtube_username, :ebay_username]
+
+  allow_public_user_custom_field :collection_thread
+  allow_public_user_custom_field :wtb_thread
+  allow_public_user_custom_field :instagram_username
+  allow_public_user_custom_field :youtube_username
+  allow_public_user_custom_field :ebay_username
+
+  add_to_serializer(:post, :collection_thread) do
+      object.user.custom_fields['collection_thread'] if object.user
+  end
+  add_to_serializer(:post, :wtb_thread) do
+      object.user.custom_fields['wtb_thread'] if object.user
+  end
+
+  add_to_serializer(:post, :instagram_username) do
+      object.user.custom_fields['instagram_username'] if object.user
+  end
+  add_to_serializer(:post, :youtube_username) do
+      object.user.custom_fields['youtube_username'] if object.user
+  end
+  add_to_serializer(:post, :ebay_username) do
+    object.user.custom_fields['ebay_username'] if object.user
+  end
+end
+
+register_asset "javascripts/discourse/templates/connectors/user-custom-preferences/user-link-preferences.hbs"
+register_asset "stylesheets/common/common.scss"
+register_asset "stylesheets/mobile/mobile.scss", :mobile
